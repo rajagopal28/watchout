b3f0078201deeda78b880bb34cec508d469c8fb9
unknown <mdivyaa.23@gmail.com>
2015-08-12 19:21:32 +0530

    Made changes to TV shows Genres to fetch data from network

--- www/js/services/tvshow-services.js
+++ www/js/services/tvshow-services.js
@@ -1,68 +1,25 @@
 angular.module('watchout.tvshow-services', [])
 
 .factory('TVGenres', function(){
-    var tvGenres = [{
-      "id":10759,
-      "name":"Action & Adventure"
-   },
-   {
-      "id":16,
-      "name":"Animation"
-   },
-   {
-      "id":35,
-      "name":"Comedy"
-   },
-   {
-      "id":99,
-      "name":"Documentary"
-   },
-   {
-      "id":18,
-      "name":"Drama"
-   },
-   {
-      "id":10751,
-      "name":"Family"
-   },
-   {
-      "id":10762,
-      "name":"Kids"
-   },
-   {
-      "id":9648,
-      "name":"Mystery"
-   },
-   {
-      "id":10763,
-      "name":"News"
-   },
-   {
-      "id":10764,
-      "name":"Reality"
-   },
-   {
-      "id":10765,
-      "name":"Sci-Fi & Fantasy"
-   },
-   {
-      "id":10766,
-      "name":"Soap"
-   },
-   {
-      "id":10767,
-      "name":"Talk"
-   },
-   {
-      "id":10768,
-      "name":"War & Politics"
-   },
-   {
-      "id":37,
-      "name":"Western"
-   }];
+    var tvGenres = [];
    var selectedTVGenres = [];
  return {
+  init : function(scope) {
+    tvGenres = [];
+    theMovieDb.genres.getList({}, 
+      function(data) {
+        var parsedData = JSON.parse(data);
+        tvGenres = parsedData.genres;
+        if(scope) {          
+          scope.tvGenres = tvGenres;
+          scope.hideSpinner();
+        }
+      }, 
+      function(error){
+        console.log('Error CB');
+        console.log(error);
+      });
+  },
   all: function() {
       for (var i = 0; i < tvGenres.length; i++) {
         for (var j=0; j< selectedTVGenres; j++) {
