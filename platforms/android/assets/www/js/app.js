c5c34d61d7bec0f8db3a36dc0c044ac5a4373290
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2015-08-28 16:51:20 +0530

    Segregated the tvshow controllers to 2 for clarity - fixed the search to show details page fix

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('watchout', ['ionic', 'ngCordova','watchout.common-services','watchout.movie-services','watchout.tvshow-services','watchout.main-controllers','watchout.movie-controllers','watchout.tvshow-controllers'])
+angular.module('watchout', ['ionic', 'ngCordova','watchout.common-services','watchout.movie-services','watchout.tvshow-services','watchout.main-controllers','watchout.movie-controllers','watchout.tvshow-controllers','watchout.tvshow-list-controllers'])
 
 .run(function($ionicPlatform, $cordovaSQLite) {
   $ionicPlatform.ready(function() {
